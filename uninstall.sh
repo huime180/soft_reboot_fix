dff9eadc1ecc3775884916c1e44a2a87e03fd63a
huime <116278145+huime180@users.noreply.github.com>
2026-04-12 03:46:09 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,3 +2,5 @@ if pm list packages | grep -q "com.tencent.tmgp.sgame"; then
     APP_DIR=$(pm path com.tencent.tmgp.sgame 2>/dev/null | head -1 | cut -d: -f2 | sed 's/\/base\.apk$//')
     chmod 755 "$APP_DIR/lib/arm64/libQAPECSharp.qti.so"
 fi
+
+pm unhide com.oplus.ota com.android.updater 2>/dev/null
