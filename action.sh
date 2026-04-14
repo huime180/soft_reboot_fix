628da3a3d3ce0c02d82bd39f7d878b607dfeb423
huime <116278145+huime180@users.noreply.github.com>
2026-04-14 16:54:10 +0800

    Add files via upload

--- action.sh
+++ action.sh
@@ -1,27 +1,29 @@
 #!/system/bin/sh
-
+MODDIR=${0%/*}
 echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 echo "— 请选择操作："
-echo "— [ 音量 加(+): 执行清理风险文件(会转移到download) ]"
-echo "— [ 音量 减(-): 对另一台设备宽容(需要双头Type-C线) ]"
-echo "— [ 等待超时(可以直接退出)：切换SElinux状态 ]"
+echo "— [ 音量 加(+): 切换USB/WIFI ADB状态 ]"
+echo "— [ 音量 减(-)：切换SElinux状态 ]"
+echo "— [ 等待超时 : 对另一台设备宽容(需要双头Type-C线) ]"
 
 START_TIME=$(date +%s)
 while true; do
   NOW_TIME=$(date +%s)
   timeout 1 getevent -lc 1 2>&1 | grep KEY_VOLUME > "$TMPDIR/events"
 
-  if [ $(( NOW_TIME - START_TIME )) -gt 3 ]; then
-    echo "— 超时未检测到按键，切换宽容模式..."
-    sh /data/adb/modules/soft_reboot_fix/bin/switch_selinux.sh
+  if [ $(( NOW_TIME - START_TIME )) -gt 5 ]; then
+    echo "— 超时未检测到按键，正在对另一台设备宽容..."
+    sh $MODDIR/bin/fb_selinux_0.sh
     break
   elif grep -q KEY_VOLUMEUP "$TMPDIR/events"; then
-    echo "— 检测到音量加键 → 执行清理风险文件"
-    sh /data/adb/modules/soft_reboot_fix/bin/clear_risk_file.sh
+    echo "— 检测到音量加键 → 开始切换USB/WIFI ADB状态..."
+    sh $MODDIR/bin/switch_adb.sh
+    sh $MODDIR/bin/write_description.sh
     break
   elif grep -q KEY_VOLUMEDOWN "$TMPDIR/events"; then
-    echo "— 检测到音量减键 → 对另一台手机宽容"
-    sh /data/adb/modules/soft_reboot_fix/bin/fb_selinux_0.sh
+    echo "— 检测到音量减键 → 开始切换SElinux状态..."
+    sh $MODDIR/bin/switch_selinux.sh
+    sh $MODDIR/bin/write_description.sh
     break
   fi
 done
